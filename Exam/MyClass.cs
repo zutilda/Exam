7b6e6a0690991a6fe21120fd3ca7f9b9a46c873c
Настя Жулина <julina.nastia2017@yandex.ru>
2022-09-06 16:37:25 +0300

    вывод в файл отладки и итогового результата

---
+++ Exam/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Exam
+{
+    public class MyClass
+    {
+        public void conclusionFile(double res)
+        {
+            StreamWriter sw = new StreamWriter("Result.txt");
+            sw.WriteLine(res);
+            sw.Close();
+        }
+
+    }
+}
--- Exam/Program.cs
+++ Exam/Program.cs
@@ -1,13 +1,45 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace Exam // Note: actual namespace depends on the project name.
 {
     public class Program
     {
+        double betBank = 0.08;
+
+       
+
         static void Main(string[] args)
         {
+         while(true)
+            {
+
+                conclusionDebag("Начало работы программы");
+                Console.Write("Хотите начать работу программы? Введите 'да' или 'нет' (ввод чувствителен к регистру):");
+                string answer = Console.ReadLine();
+                if (answer=="да")
+                {
+                   
+                }
+                else Environment.Exit(0);
+            }
 
         }
+
+        public static void conclusionDebag(string diagnostic)
+        {
+            string location = Environment.CurrentDirectory;
+            location = location.Insert(location.Length, "Diagnostic.txt");
+            FileStream file = new FileStream(location, FileMode.Create);
+            file.Close();
+            TextWriterTraceListener tr = new TextWriterTraceListener(File.AppendText("Diagnostic.txt"));
+            Trace.Listeners.Add(tr);
+            Debug.WriteLine(diagnostic);
+            Debug.Flush();
+            tr.Close();
+        }
+
+       
     }
 }
