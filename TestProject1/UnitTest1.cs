bdc66dc3c3bf8c231a2b51f99df6d1521eba8c85
ЖулинаАА <@101-09>
2022-09-06 15:27:00 +0300

    Добавьте файлы проекта.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Exam;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
