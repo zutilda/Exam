c7d8dfdc1874d935c0ad9bbfb3f0097e206f82fc
Настя Жулина <julina.nastia2017@yandex.ru>
2022-09-06 17:07:49 +0300

    Добавление автодокументирования, метод рассчета процентов

--- Exam/MyClass.cs
+++ Exam/MyClass.cs
@@ -9,6 +9,10 @@ namespace Exam
 {
     public class MyClass
     {
+        /// <summary>
+        /// Метод записывает результат вычислений  в отдельный файл
+        /// </summary>
+        /// <param name="res"> результат по расчету вклада из главного метода программы</param>
         public void conclusionFile(double res)
         {
             StreamWriter sw = new StreamWriter("Result.txt");
--- Exam/Program.cs
+++ Exam/Program.cs
@@ -5,32 +5,64 @@ using System.IO;
 namespace Exam // Note: actual namespace depends on the project name.
 {
     public class Program
-    {
-        double betBank = 0.08;
+    {        
+        private static double interestСalculation(double cash)
+        {
+            double money = 0;
+            double procent = 0;
+            while (money <= cash)
+            {
+                if (cash >= 700000 && cash <= 749999.99)
+                {
+                    procent = 0.2;
+                    break;
+                }
 
-       
+                if (money == 700000)
+                {
+                    procent = 0.2;
+                    money += 50000;
+                    continue;
+                }
+
+                if (money < 750000)
+                {
+                    money += 49999.99;
+                    procent += 0.01;
+                    money += 0.01;
+                }
+                else
+                {
+                    money += 49999.99;
+                    procent -= 0.01;
+                    money += 0.01;
+                }
+            }
+            return Math.Round(procent, 2);
+        }
 
         static void Main(string[] args)
         {
          while(true)
             {
-
                 conclusionDebag("Начало работы программы");
-                Console.Write("Хотите начать работу программы? Введите 'да' или 'нет' (ввод чувствителен к регистру):");
-                string answer = Console.ReadLine();
-                if (answer=="да")
-                {
-                   
+                
+                    MyClass obj = new MyClass();
+                    obj.conclusionFile(res);
                 }
                 else Environment.Exit(0);
             }
 
         }
 
+        /// <summary>
+        /// Метод записывает информацию об отладке в файл
+        /// </summary>
+        /// <param name="diagnostic">строка передающаяся для записи в отладочный файл</param>
         public static void conclusionDebag(string diagnostic)
         {
             string location = Environment.CurrentDirectory;
-            location = location.Insert(location.Length, "Diagnostic.txt");
+            location = location.Insert(location.Length, "Diagnostic.txt"); // указание пути к файлу в который будет записываться отладочная информация
             FileStream file = new FileStream(location, FileMode.Create);
             file.Close();
             TextWriterTraceListener tr = new TextWriterTraceListener(File.AppendText("Diagnostic.txt"));
